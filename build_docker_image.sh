d827469fa9e4e3e1d411f371c1204a1f00d75592
Scott Boone <scott.boone@8x8.com>
2021-02-11 14:57:24 -0600

    force remove main

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 main() {
-    rm ./main
+    rm -f ./main
     CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -o main ./cmd/api/
     docker build -t jitsi/slack-integration:$TAG .
 }
